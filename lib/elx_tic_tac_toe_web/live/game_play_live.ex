a15897b37411750a45f78ce7e6e816ff864693db
Tiago Avila <tiago.avila09@gmail.com>
2024-03-13 18:13:33 -0300

    Added Phoenix.PubSub to update game page when 2nd user joins

--- lib/elx_tic_tac_toe_web/live/game_play_live.ex
+++ lib/elx_tic_tac_toe_web/live/game_play_live.ex
@@ -1,10 +1,19 @@
 defmodule ElxTicTacToeWeb.GamePlayLive do
   use ElxTicTacToeWeb, :live_view
 
-  alias ElxTicTacToe.{GameState, GameServer}
+  alias ElxTicTacToe.GameServer
+  alias Phoenix.PubSub
 
   def mount(%{"game" => game_code, "player" => player_id}, _session, socket) do
+    if connected?(socket),
+      do: PubSub.subscribe(ElxTicTacToe.PubSub, "game-#{game_code}")
+
     game_state = GameServer.get_current_state(game_code)
     {:ok, socket |> assign(game_code: game_code, player_id: player_id, game: game_state)}
+  end
+
+  def handle_info(:game_state_updated, socket) do
+    game_state = GameServer.get_current_state(socket.assigns.game_code)
+    {:noreply, assign(socket, game: game_state)}
   end
 end
--- lib/elx_tic_tac_toe_web/live/home_live.ex
+++ lib/elx_tic_tac_toe_web/live/home_live.ex
@@ -2,6 +2,7 @@ defmodule ElxTicTacToeWeb.HomeLive do
   use ElxTicTacToeWeb, :live_view
 
   alias ElxTicTacToe.{GameStarter, Player, GameServer}
+  alias Phoenix.PubSub
 
   def mount(_params, _session, socket) do
     {:ok, socket |> assign(:changeset, GameStarter.starting_changeset())}
@@ -19,8 +20,17 @@ defmodule ElxTicTacToeWeb.HomeLive do
   def handle_event("submit_game", %{"game_starter" => params}, socket) do
     with {:ok, game} <- GameStarter.create(params),
          {:ok, player} <- Player.create(params),
-         {:ok, _} <- GameServer.start_or_join_game(game.game_code, player) do
+         {:ok, game_event} <- GameServer.start_or_join_game(game.game_code, player) do
       IO.puts("Game create #{game.game_code} - player #{player.id}")
+
+      if game_event == :joined,
+        do:
+          PubSub.broadcast(
+            ElxTicTacToe.PubSub,
+            "game-#{game.game_code}",
+            :game_state_updated
+          )
+
       {:noreply, push_navigate(socket, to: ~p"/play?game=#{game.game_code}&player=#{player.id}")}
     else
       {:error, changeset} ->
