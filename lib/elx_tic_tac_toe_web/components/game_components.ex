cfa231059eb740a20bb9bd06db55cec4bafdec0e
Tiago Avila <tiago.avila09@gmail.com>
2024-03-20 18:04:44 -0300

    improved UI and added custom component for reset game button

--- lib/elx_tic_tac_toe_web/components/game_components.ex
+++ lib/elx_tic_tac_toe_web/components/game_components.ex
@@ -2,6 +2,8 @@ defmodule ElxTicTacToeWeb.Layouts.GameComponents do
   @moduledoc """
   This module is responsible for managing the game components in the ElxTicTacToe application.
   """
+  use Phoenix.Component
+
   alias Phoenix.LiveView.JS
 
   def get_current_player_class(current_player, player_id, game) do
@@ -19,5 +21,17 @@ defmodule ElxTicTacToeWeb.Layouts.GameComponents do
   @spec send_move(String.t(), String.t(), integer()) :: no_return
   def send_move(current_player, game_code, square) do
     JS.push("send_move", value: %{square: square, current_player: current_player, game_code: game_code})
+  end
+
+  attr :game_code, :string, required: true
+  def reset_game_button(assigns) do
+    ~H"""
+    <button
+      class="mt-4 px-4 py-2 bg-blue-500 text-white font-bold rounded hover:bg-blue-600 transition ease-in-out duration-150"
+      phx-click={JS.push("reset_game", value: %{game_code: @game_code})}
+    >
+      Play again
+    </button>
+    """
   end
 end
